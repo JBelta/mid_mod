5b0d7cde678b292561971bad8d10b5d516c02c00
James Belta <james.belta@gmail.com>
2020-04-02 10:00:08 -0600

    Added assertions to test. Year, make and model

--- test/vehicle_test.rb
+++ test/vehicle_test.rb
@@ -8,5 +8,8 @@ class VehicleTest < Minitest::Test
   def test_it_exist
     vehicle = Vehicle.new("2001", "Honda", "Civic")
     assert_instance_of Vehicle, vehicle
+    assert_equal "2001", vehicle.year
+    assert_equal "Honda", vehicle.make
+    assert_equal "Civic", vehicle.model
   end
 end
