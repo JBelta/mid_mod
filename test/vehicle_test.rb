ef7f17c93f769acf72987babbabd5300e1606107
James Belta <james.belta@gmail.com>
2020-04-02 09:57:30 -0600

    Passed instance assertion test

--- test/vehicle_test.rb
+++ test/vehicle_test.rb
@@ -1,7 +1,12 @@
 require 'minitest/autorun'
 require 'minitest/pride'
-require './lib/Passenger'
+require './lib/passenger'
 require './lib/vehicle'
 
 class VehicleTest < Minitest::Test
+
+  def test_it_exist
+    vehicle = Vehicle.new("2001", "Honda", "Civic")
+    assert_instance_of Vehicle, vehicle
+  end
 end
