7364cecb44f113b81605d738b5d3db2756505d49
James Belta <james.belta@gmail.com>
2020-04-02 09:17:46 -0600

    Assert taylor instance

--- test/passenger_test.rb
+++ test/passenger_test.rb
@@ -6,6 +6,8 @@ class PassengerTest < Minitest::Test
 
   def test_it_exist
     charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    taylor = Passenger.new({"name" => "Taylor", "age" => 12})
     assert_instance_of Passenger, charlie
+    assert_instance_of Passenger, taylor
   end
 end
